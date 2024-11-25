8d3347b1bc17c66ba86ce32e456692688d290726
toshihiro ozeki <you@example.com>
2024-11-25 17:28:24 +0900

    add: main() subject_test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,6 +5,22 @@
 int main()
 {
 	{
+		int a = 2;
+		int b = 3;
+		ft::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ft::min(a, b) << std::endl;
+		std::cout << "max( a, b ) = " << ft::max(a, b) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		ft::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ft::min(c, d) << std::endl;
+		std::cout << "max( c, d ) = " << ft::max(c, d) << std::endl;
+	}
+	std::cout << std::endl;
+
+	{
 		int a = 1;
 		int b = 100;
 		std::cout << "min: " << ft::min(a, b) << " max: " << ft::max(a, b) << std::endl;
@@ -13,6 +29,7 @@ int main()
 		std::cout << "after_swap a: " << a << std::endl;
 	}
 	std::cout << std::endl;
+
 	{
 		std::string a = "abcde-f";
 		std::string b = "abcde-z";
