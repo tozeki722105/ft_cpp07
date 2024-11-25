0b415fb03ff8b0596798a2e635dda6c176040351
toshihiro ozeki <you@example.com>
2024-11-25 18:12:16 +0900

    fix: maxで同値の際、bを返すよう修正

--- ex00/template.hpp
+++ ex00/template.hpp
@@ -13,7 +13,7 @@ void swap(T &a, T &b)
 template <typename T>
 T max(const T &a, const T &b)
 {
-	return (a >= b) ? a : b;
+	return (a > b) ? a : b;
 }
 template <typename T>
 T min(const T &a, const T &b)
