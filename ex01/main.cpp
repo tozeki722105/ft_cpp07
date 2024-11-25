cbb5f375cd27180432a4c5af32447b77e2f865fd
toshihiro ozeki <you@example.com>
2024-11-25 18:10:34 +0900

    fix&add: test関数をmainに移動 testケース作成

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,47 +1,38 @@
 #include "template.hpp"
 
-void aaa(std::string& c)
+namespace test
 {
-	c = "aaa";
-}
-
-template <typename T1>
-void test(T1& a)
+template <typename T>
+void add32(T &val)
 {
-	std::cout << "test" << std::endl;
-	a = "aaaaaaaaaaaaa";
-	// std::cout << a << std::endl;
+	val += 32;
 }
-
-template <typename T1>
-void test(const T1& a)
-{
-	std::cout << "const" << " test" << std::endl;
-	std::cout << a << std::endl;
-}
-
-template <typename T1>
-void test1(const T1& a)
-{
-	std::cout << "const" << " test" << std::endl;
-	std::cout << a << std::endl;
-}
-
-template <typename T1>
-void test1(T1& a)
-{
-	std::cout << "test1" << std::endl;
-	// std::cout << a << std::endl;
-	a += 1;
-}
+}  // namespace test
 
 int main()
 {
-	// std::string str = "abcdefg";
-	// ft::iter(str, str.length(), aaa);
-	int array[3] = {0, 1, 2};
-	ft::iter(array, 3, test1);
-	for (int i = 0; i < 3; i++) {
-		std::cout << i << " " << array[i] << std::endl;
+	{
+		size_t size = 3;
+		int array[3] = {0, 1, 2};
+		ft::iter(array, size, test::add32);
+		for (size_t i = 0; i < size; i++) {
+			std::cout << i << ": " << array[i] << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::string str = "ABCDEFG";
+		ft::iter(&str[0], str.length(), test::add32);
+		for (size_t i = 0; i < str.length(); i++) {
+			std::cout << i << " " << str[i] << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::string str = "ABCDEFG";
+		ft::iter<char>(&str[0], str.length(), NULL);
+		for (size_t i = 0; i < str.length(); i++) {
+			std::cout << i << " " << str[i] << std::endl;
+		}
 	}
 }
