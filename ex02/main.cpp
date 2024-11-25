25045153ca213361b55c895b19d17450df97fd9b
Toshihiro Ozeki <tozeki@c3r6s3.42tokyo.jp>
2024-11-25 20:48:20 +0900

    add: test const用の[]オーバーロードの必要性を感じないがとりあえず残しておく

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,17 +2,90 @@
 
 #include "Array.hpp"
 
+#define COMMON_SIZE 3
+
 int main()
 {
+	std::cout << "<<int>>" << std::endl;
 	{
-		unsigned int size = 10;
-		Array<int> iArray(size);
-		iArray[0] = 100;
-		iArray[2] = 100;
-		for (unsigned int i = 0; i < size; i++) {
-			std::cout << iArray[i] << std::endl;
+		try {
+			std::cout << "output test" << std::endl;
+			Array<int> iArray(COMMON_SIZE);
+			iArray[0] = 100;
+			iArray[2] = 100;
+			for (unsigned int i = 0; i < iArray.size(); i++) {
+				std::cout << i << ": " << iArray[i] << std::endl;
+			}
+
+			std::cout << "copy test" << std::endl;
+			Array<int> cpIArray(iArray);
+			for (unsigned int i = 0; i < cpIArray.size(); i++) {
+				std::cout << i << ": " << cpIArray[i] << std::endl;
+			}
+
+			std::cout << "equal test" << std::endl;
+			Array<int> eqIArray(3);
+			eqIArray = cpIArray;
+			for (unsigned int i = 0; i < eqIArray.size(); i++) {
+				std::cout << i << ": " << eqIArray[i] << std::endl;
+			}
+			std::cout << "test end" << std::endl;	
+		} catch (std::exception &e) {
+			std::cout << "error: " << e.what() << std::endl;
 		}
 	}
+	std::cout << "\n<<string class>>" << std::endl;
+	{
+		try {
+			std::cout << "output test" << std::endl;
+			Array<std::string> sArray(COMMON_SIZE);
+			sArray[0] = "test";
+			sArray[2] = "test test";
+			for (unsigned int i = 0; i < sArray.size(); i++) {
+				std::cout << i << ": " << sArray[i] << std::endl;
+			}
+			
+			std::cout << "copy test" << std::endl;
+			Array<std::string> cpSArray(sArray);
+			for (unsigned int i = 0; i < cpSArray.size(); i++) {
+				std::cout << i << ": " << cpSArray[i] << std::endl;
+			}
 
-	system("leaks -q a.out");
+			std::cout << "equal test" << std::endl;
+			Array<std::string> eqSArray(3);
+			eqSArray = cpSArray;
+			for (unsigned int i = 0; i < eqSArray.size(); i++) {
+				std::cout << i << ": " << eqSArray[i] << std::endl;
+			}
+			std::cout << "test end" << std::endl;
+		} catch (std::exception &e) {
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << "\n<<const string class>>" << std::endl;
+	{
+		try {
+			std::cout << "output test" << std::endl;
+			Array<const std::string> csArray(COMMON_SIZE);
+			for (unsigned int i = 0; i < csArray.size(); i++) {
+				std::cout << i << ": " << csArray[i] << std::endl;
+			}
+			
+			// std::cout << "copy test" << std::endl;
+			// Array<const std::string> cpCsArray(csArray);
+			// for (unsigned int i = 0; i < cpCsArray.size(); i++) {
+			// 	std::cout << i << ": " << cpCsArray[i] << std::endl;
+			// }
+
+			// std::cout << "equal test" << std::endl;
+			// Array<const std::string> eqCsArray(3);
+			// eqCsArray = cpCsArray;
+			// for (unsigned int i = 0; i < eqCsArray.size(); i++) {
+			// 	std::cout << i << ": " << eqCsArray[i] << std::endl;
+			// }
+			std::cout << "test end" << std::endl;
+		} catch (std::exception &e) {
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
 }
